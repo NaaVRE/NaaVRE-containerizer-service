25da2f024ad7c8a5c505819ceef1215e1aa1c344
skoulouzis <koulouzis@gmail.com>
2025-02-01 20:42:39 +0100

    rename settings

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 from app.models.vl_config import VLConfig
 
 
-class WorkflowServiceSettings(BaseSettings):
+class ServiceSettings(BaseSettings):
     root_path: str
     debug: bool
     vl_configurations: List[VLConfig]
@@ -17,13 +17,13 @@ class Settings:
         self.config_file = config_file
         self.service_settings = self.load_config()
 
-    def load_config(self) -> WorkflowServiceSettings:
+    def load_config(self) -> ServiceSettings:
         """
         Load the configuration from the json file and parse it into VLConfig.
         """
         with open(self.config_file, 'r') as file:
             config_data = json.load(file)
-        return WorkflowServiceSettings(**config_data)
+        return ServiceSettings(**config_data)
 
     def get_vl_config(self, virtual_lab) -> VLConfig:
         for setting in self.service_settings.vl_configs:
