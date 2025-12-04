515ef3717610a9a1d2e087a4597ef68058549ca1
skoulouzis <koulouzis@gmail.com>
2025-12-04 20:59:29 +0100

    latest

--- app/tests/test_extract_cell.py
+++ app/tests/test_extract_cell.py
@@ -176,7 +176,7 @@ def get_latest_container_tags_from_ghcr_url(ghcr_url, token):
 
     Args:
         ghcr_url (str): e.g.
-            'ghcr.io/qcdis/naavre/naavre-cell-build-python:latest'
+            'ghcr.io/naavre/flavors/naavre-fl-vanilla-cell-build:latest'
         token (str): GitHub token with 'read:packages' permission
 
     Returns:
